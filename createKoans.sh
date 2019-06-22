823d0032d07b939d78b64214bf2e21a27f8b9115
David Whitlock <david.m.whitlock@gmail.com>
2019-06-22 07:09:10 -0700

    Added script for creating the Koans project

---
+++ createKoans.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+source checkForJava.sh
+
+if ! checkForJava; then
+    echo "** Java is not configured correctly in your environment"
+    exit 1
+fi
+
+if ! checkForMavenSettings; then
+    echo "** Your Maven settings are not configured correctly in your environment"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "** Missing your student id"
+    exit 1
+fi
+
+studentId=$1
+
+./mvnw --batch-mode archetype:generate \
+  -DinteractiveMode=false \
+  -DarchetypeGroupId=edu.pdx.cs410J \
+  -DarchetypeArtifactId=java-koans-archetype \
+  -DgroupId=edu.pdx.cs410J.${studentId} \
+  -DartifactId=koans \
+  -Dpackage=edu.pdx.cs410J.${studentId} \
+  -Dversion=Summer2019
