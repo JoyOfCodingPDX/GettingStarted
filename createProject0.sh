7aebf3735e9c3e04e928e240c3f6850e86b62dab
David Whitlock <david.m.whitlock@gmail.com>
2019-06-27 20:07:39 -0700

    Refactor the code for creating a simple student project into its own script.  This will make it easier to create new Maven projects for the katas

--- createProject0.sh
+++ createProject0.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 
-source checkForJava.sh
-
-if ! checkForJava; then
-    echo "** Java is not configured correctly in your environment"
-    exit 1
-fi
-
-if ! checkForMavenSettings; then
-    echo "** Your Maven settings are not configured correctly in your environment"
-    exit 1
-fi
-
-if [ "$#" -ne 1 ]; then
-    echo "** Missing your student id"
-    exit 1
-fi
-
 studentId=$1
-
-./mvnw --batch-mode archetype:generate \
-  -DinteractiveMode=false \
-  -DarchetypeGroupId=edu.pdx.cs410J \
-  -DarchetypeArtifactId=student-archetype \
-  -DgroupId=edu.pdx.cs410J.${studentId} \
-  -DartifactId=student \
-  -Dpackage=edu.pdx.cs410J.${studentId} \
-  -Dversion=Summer2019
+source createSimpleProject.sh "student" $studentId
---
+++ createSimpleProject.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source checkForJava.sh
+
+if ! checkForJava; then
+    echo "** Java is not configured correctly in your environment"
+    exit 1
+fi
+
+if ! checkForMavenSettings; then
+    echo "** Your Maven settings are not configured correctly in your environment"
+    exit 1
+fi
+
+if [ $# -lt 1 ]; then
+  echo "** You must specify a project name and a student id"
+  exit 1
+
+elif [ $# -lt 2 ]; then
+  echo "** Missing your student id"
+  exit 1
+fi
+
+
+projectName=$1
+studentId=$2
+
+./mvnw --batch-mode archetype:generate \
+  -DinteractiveMode=false \
+  -DarchetypeGroupId=edu.pdx.cs410J \
+  -DarchetypeArtifactId=student-archetype \
+  -DarchetypeVersion=Summer2019 \
+  -DgroupId=edu.pdx.cs410J.${studentId} \
+  -DartifactId=$projectName \
+  -Dpackage=edu.pdx.cs410J.${studentId} \
+  -Dversion=Summer2019
