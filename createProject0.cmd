8981ef5f51abb0aef3ba19304e71fa74cfefb82a
David Whitlock <david.m.whitlock@gmail.com>
2020-01-15 18:38:49 -0800

    Added the archetype version to the createProject0.cmd script

--- createProject0.cmd
+++ createProject0.cmd
@@ -6,6 +6,7 @@ mvnw.cmd --batch-mode archetype:generate ^
   -DinteractiveMode=false ^
   -DarchetypeGroupId=edu.pdx.cs410J ^
   -DarchetypeArtifactId=student-archetype ^
+  -DarchetypeVersion=Winter2020 ^
   -DgroupId=edu.pdx.cs410J.%studentId% ^
   -DartifactId=student ^
   -Dpackage=edu.pdx.cs410J.%studentId% ^
