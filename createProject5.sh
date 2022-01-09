c91d36a8f0d58b7ed5c77a68a1e26bd1dfa3de3a
David Whitlock <david.m.whitlock@gmail.com>
2022-01-09 13:19:54 -0800

    Use student login id from "me.xml" when creating the REST project.

--- createProject5.sh
+++ createProject5.sh
@@ -7,19 +7,25 @@ if ! checkForJava; then
     exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
-    echo "** Missing your student id"
-    exit 1
+if [[ $# -gt 0 ]]; then
+    loginId=$1
+
+else
+    loginId=$(getLoginIdFromXmlFile)
+    if [[ -z "${loginId}" ]]; then
+    	echo "** Could not load login id from me.xml"
+    	exit 1
+    fi
 fi
 
-studentId=$1
+echo "** Creating project for ${loginId}"
 
 ./mvnw --batch-mode archetype:generate \
   -DinteractiveMode=false \
   -DarchetypeGroupId=io.github.davidwhitlock.cs410J \
   -DarchetypeArtifactId=airline-web-archetype \
   -DarchetypeVersion=2022.0.0 \
-  -DgroupId=edu.pdx.cs410J.${studentId} \
+  -DgroupId=edu.pdx.cs410J.${loginId} \
   -DartifactId=airline-web \
-  -Dpackage=edu.pdx.cs410J.${studentId} \
+  -Dpackage=edu.pdx.cs410J.${loginId} \
   -Dversion=2022.0.0
